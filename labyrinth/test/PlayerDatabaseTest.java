aa330bdb1338b297c89650d98c1bf3b1decfabcc
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-10-09 09:33:08 +0200

    ajout test PlayerDatabase + structure test PlayerDatabase

---
+++ labyrinth/test/PlayerDatabaseTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlayerDatabaseTest {
+    testSavePlayer(){
+
+    }
+
+    testLoadPlayer(){
+
+    }
+
+    testPlayerExists(){
+
+    }
+}
