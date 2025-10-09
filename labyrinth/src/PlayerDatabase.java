3ed4a5e0ef177fa5f3900576d9edb14eca4bf397
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-10-09 13:00:02 +0200

    ajout squellette PlayerDatabase

---
+++ labyrinth/src/PlayerDatabase.java
@@ -0,0 +1,23 @@
+public class PlayerDatabase{
+
+    private String file="./PlayerDatabase";
+    public PlayerDatabase(String file) {
+
+    }
+
+    public savePlayer(Player player, String file){
+
+    }
+
+    public Player loadPlayer(Player player,String file){
+
+    }
+
+    public boolean playerExists(String nom,String file){
+
+    }
+
+    public VerifyData(){
+
+    }
+}
