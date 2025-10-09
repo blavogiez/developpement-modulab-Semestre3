7fba93dcc2b19738ca5555521470c7e8c99f4088
Zheng <angel.zheng.etu@univ-lille.fr>
2025-10-09 13:26:44 +0200

    Test2 Responsive Windows

--- labyrinth/src/main/java/fr/univlille/labyrinth/HelloController.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/HelloController.java
@@ -5,10 +5,10 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label dd;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        dd.setText("Welcome to JavaFX Application!");
     }
 }
