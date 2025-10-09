fe1e0a9af2f02cc27eb747aa361cdd2512f0927f
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-10-09 13:20:55 +0200

    ajout fichier csv PlayerDatabase

--- labyrinth/src/PlayerDatabase.java
+++ labyrinth/src/PlayerDatabase.java
@@ -6,7 +6,7 @@ public class PlayerDatabase{
     }
 
     public PlayerDatabase() {
-        this.file="./PlayerDatabase";
+        this.file="./res/PlayerDatabase.csv";
     }
 
     public savePlayer(Player player){
