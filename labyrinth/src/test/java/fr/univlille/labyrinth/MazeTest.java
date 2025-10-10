f25f8647152891685f24e4a3b5d01388f0e12e21
Antonin Marouze <antonin.marouze.etu@univ-lille.fr>
2025-10-10 15:20:49 +0200

    change Maze And algo Laby

--- labyrinth/src/test/java/fr/univlille/labyrinth/MazeTest.java
+++ labyrinth/src/test/java/fr/univlille/labyrinth/MazeTest.java
@@ -1,6 +1,7 @@
 package fr.univlille.labyrinth;
 
 import fr.univlille.labyrinth.model.*;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,18 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MazeTest {
     static Maze m1,m2,m3,m4;
-    static Position p1,p2,p3;
-
     @BeforeAll
     public static void initialization(){
         m1 = new Maze(10, 10, 50);
         m2 = new Maze(20, 20, 30);
         m3 = new Maze(30, 30, 50);
         m4=m1;
-        p1=new Position(5,5);
-        p2=new Position(10,10);
-        p3=new Position(15,15);
-
     }
     @Test
     public void testGridInitialization() {
