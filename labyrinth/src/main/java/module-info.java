a1a8f5c8cd3986501c56c684039087b983cb3a50
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2025-10-11 22:17:32 +0200

    fix(tests): Pom.xml update pour permettre [mvn test] clean

--- labyrinth/src/main/java/module-info.java
+++ labyrinth/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module fr.univlille.labyrinth {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens fr.univlille.labyrinth to javafx.fxml;
+    opens fr.univlille.labyrinth to javafx.fxml, org.junit.platform.commons;
     exports fr.univlille.labyrinth;
+    exports fr.univlille.labyrinth.model;
 }
