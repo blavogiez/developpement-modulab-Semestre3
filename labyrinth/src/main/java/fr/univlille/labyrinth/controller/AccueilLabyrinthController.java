b8c5f65710740ce31f7bd926a22b187e3315109a
angel.zheng.etu <angel.zheng.etu@univ-lille.fr>
2025-10-13 08:59:26 +0200

    Changement package en cours

--- labyrinth/src/main/java/fr/univlille/labyrinth/controller/AccueilLabyrinthController.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/controller/AccueilLabyrinthController.java
@@ -1,6 +1,24 @@
 package fr.univlille.labyrinth.controller;
 
 
-public class AccueilLabyrinthController  {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class AccueilLabyrinthController  {
+    @FXML
+    private Button b1;
+
+    @FXML
+    private void changerPage() throws IOException {
+        Stage stage = (Stage) b1.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("modeLibre.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- labyrinth/src/main/java/module-info.java
+++ labyrinth/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module fr.univlille.labyrinth {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens fr.univlille.labyrinth to javafx.fxml;
     exports fr.univlille.labyrinth;
+    exports fr.univlille.labyrinth.controller;
+    opens fr.univlille.labyrinth.controller to javafx.fxml;
 }
