c28628dbcfec459c0f793ff54511e34652465940
Zheng <angel.zheng.etu@univ-lille.fr>
2025-10-14 11:22:47 +0200

    Ajout qq page fxml

---
+++ labyrinth/src/main/java/fr/univlille/labyrinth/controller/ModeLibreController.java
@@ -0,0 +1,24 @@
+package fr.univlille.labyrinth.controller;
+
+import fr.univlille.labyrinth.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ModeLibreController {
+    @FXML
+    private Button bouttonValider;
+
+    @FXML
+    private void goToModeLaby() throws IOException {
+        Stage stage = (Stage) HelloApplication.getPrimaryStage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fr/univlille/labyrinth/LabyrinthModeLibre.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
