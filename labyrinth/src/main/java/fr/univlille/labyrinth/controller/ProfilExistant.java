ac1d1cd63387c0e1dfec28885e63eccd9be63f91
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-10-15 14:42:25 +0200

    ajout controlleur java  ProfilExistant

--- labyrinth/src/main/java/fr/univlille/labyrinth/controller/ProfilExistant.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/controller/ProfilExistant.java
@@ -1,4 +1,32 @@
 package fr.univlille.labyrinth.controller;
 
+import fr.univlille.labyrinth.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class ProfilExistant {
+    private Button bouttonNouvellePartie;
+    @FXML
+    private Button bouttonReprendre;
+    @FXML
+    private Button bouttonRetour;
+
+    @FXML
+    private void goToNewProgression() throws IOException { //nouvelle partie
+        HelloApplication.goTo("Progression.fxml");
+    }
+
+
+
+    @FXML
+    private void goToProgression() throws IOException { //reprendre partie
+        HelloApplication.goTo("Progression.fxml");
+    }
+
+    @FXML
+    private void goToProgressionEntreNom() throws IOException {
+        HelloApplication.goTo("ProgressionEntreNom.fxml");
+    }
 }
