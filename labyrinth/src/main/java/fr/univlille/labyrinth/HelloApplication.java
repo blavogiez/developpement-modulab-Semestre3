ddd63ac630a71b64dabcfdabafbb3fc2f77dca6c
Baptiste Lavogiez <baptiste.lavogiez.etu@univ-lille.fr>
2025-10-15 11:51:39 +0200

    feat(javafx): integration en marche

--- labyrinth/src/main/java/fr/univlille/labyrinth/HelloApplication.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/HelloApplication.java
@@ -2,14 +2,25 @@ package fr.univlille.labyrinth;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import fr.univlille.labyrinth.controller.LabyrinthControler;
+import fr.univlille.labyrinth.model.* ;
+import fr.univlille.labyrinth.view.LabyrinthScene;
+
 public class HelloApplication extends Application {
 
+    private GameMode gameMode;
+
+    public GameMode getGameMode() {
+        return gameMode;
+    }
     private static Stage primaryStage;
 
     public static void goTo(String page) throws IOException {
@@ -21,11 +32,38 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        primaryStage = stage;
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+        final double minHeigth = screen.getHeight()/1.5;
+        final double minWidth = screen.getWidth()/1.5;
+
+        stage.setOnShown(x ->{
+            stage.setMinHeight(minHeigth);
+            stage.setMinWidth(minWidth);
+        });
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AccueilLabyrinth.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        
         stage.setScene(scene);
+
+        stage.setX(screen.getMinX());
+        stage.setY(screen.getMinY());
+        stage.setWidth(screen.getWidth());
+        stage.setHeight(screen.getHeight());
+        stage.setTitle("Labyrinthe");
+        stage.show();
+
+        primaryStage = stage;
+
+        //À METTRE DANS UN BOUTON
+        this.gameMode=new FreeMode();
+        gameMode.setCurrentMaze(null);
+        goTo(new LabyrinthScene(null));
+        null.setControler(new LabyrinthControler(this));
+
+
+        //
+        
         stage.show();
     }
 
@@ -35,5 +73,15 @@ public class HelloApplication extends Application {
 
     public static void goTo(Scene scene){
         primaryStage.setScene(scene);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+
+        LabyrinthScene labyScene = new LabyrinthScene(gameMode.getCurrentMaze());
+        labyScene.setControler(new LabyrinthControler(this));
+        gameMode.getCurrentMaze().add(labyScene);
+        this.gameMode.start();
     }
 }
--- labyrinth/src/main/java/fr/univlille/labyrinth/Main.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/Main.java
@@ -12,36 +12,10 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private static AlgoLabyOld algo;
-
-
-
-    public static AlgoLabyOld getAlgo() {
-        if (algo==null) algo = new AlgoLabyOld();
-        return algo;
-    }
-    public static Main instance;
-
-    public GameMode startGame() {
-        //The Main menu's controller need to interact with this. It supposed to make a FreeMode, or a Progression mode, and push a Scene (ex : scenes.push(new FreeMenuScene()) )
-        return null;
-    }
-
     private GameMode gameMode;
 
     public GameMode getGameMode() {
         return gameMode;
-    }
-
-    private SceneManager scenes;
-
-    public void displayProgress(Player player) {
-
-    }
-
-    public void savePlayer(Player player) {
-
     }
 
     @Override
@@ -55,8 +29,6 @@ public class Main extends Application {
             stage.setMinWidth(minWidth);
         });
 
-
-
         stage.setX(screen.getMinX());
         stage.setY(screen.getMinY());
         stage.setWidth(screen.getWidth());
@@ -64,41 +36,21 @@ public class Main extends Application {
         stage.setTitle("Labyrinthe");
         stage.show();
 
-        this.algo=new AlgoLabyOld();
-        this.scenes = new SceneManager(stage, new Scene(new Pane()));
-        this.instance=this;
         this.gameMode=new GameMode() {
             @Override
             public void start() {
-                gameMode.setCurrentMaze(new Maze(11,11,0.5));
+                gameMode.setCurrentMaze(new Maze(11,21,0.5));
                 LabyrinthScene labyScene = new LabyrinthScene(gameMode.getCurrentMaze());
                 labyScene.setControler(new LabyrinthControler(this));
-                Main.getInstance().getScenes().push(labyScene);
                 gameMode.getCurrentMaze().add(labyScene);
-
             }
 
             @Override
             public void playerWin() {
                 System.out.println("Le joueur à win !");
-                scenes.pop();
+                // toto
             }
         };
         this.gameMode.start();
-
-
-
-
-
-
-    }
-
-    public static Main getInstance() {
-        return instance;
-    }
-    //Main is a Singleton. To call out method, you need to write "Main.getInstance().<the method you want>". It's like a static object but more optimised :D
-
-    public SceneManager getScenes() {
-        return scenes;
     }
 }
