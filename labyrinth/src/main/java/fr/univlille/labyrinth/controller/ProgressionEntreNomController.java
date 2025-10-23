adc50aea69ff0d6a5476765ad006f23b0577eacc
victor bredelle <victor.bredelle.etu@univ-lille.fr>
2025-10-23 11:08:40 +0200

    change

--- labyrinth/src/main/java/fr/univlille/labyrinth/controller/ProgressionEntreNomController.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/controller/ProgressionEntreNomController.java
@@ -2,33 +2,20 @@ package fr.univlille.labyrinth.controller;
 
 import fr.univlille.labyrinth.Main;
 import fr.univlille.labyrinth.model.save.PlayerDatabase;
-
-import fr.univlille.labyrinth.view.Redimension;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class ProgressionEntreNomController {
-    // nom rentré par le joueur
-    // + valeur par défaut si rien n'est entré
     public static String playerName = "toto";
 
     @FXML
     private TextField nameField;
 
-    @FXML
-    private VBox menu;
-
-    /** 
+    /**
      * @throws IOException
      */
-    @FXML
-    public void initialize(){
-        menu.widthProperty().addListener((o, oldW, newW) -> Redimension.redimensionnerVboxControles(menu));
-        menu.heightProperty().addListener((o, oldH, newH) -> Redimension.redimensionnerVboxControles(menu));
-    }
     @FXML
     private void goToProgression() throws IOException {
         // Collecte de la saisie de l'utilisateur + traitement
@@ -37,13 +24,13 @@ public class ProgressionEntreNomController {
             playerName = name.trim();
         }
         if (PlayerDatabase.playerExists(playerName)) {
-            Main.goTo("ProfilExistantController.fxml");
+            Main.goTo("ProfilExistant.fxml");
         } else {
             Main.goTo("Progression.fxml");
         }
     }
 
-    /** 
+    /**
      * @throws IOException
      */
     @FXML
