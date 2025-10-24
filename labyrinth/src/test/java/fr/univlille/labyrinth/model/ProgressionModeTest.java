1d03d6545c599b15e19c25cec22646f3aa9026f8
Baptiste Lavogiez <baptiste.lavogiez@proton.me>
2025-10-24 17:22:49 +0200

    test(progressionmode): Test suite au manque de couverture

---
+++ labyrinth/src/test/java/fr/univlille/labyrinth/model/ProgressionModeTest.java
@@ -0,0 +1,89 @@
+package fr.univlille.labyrinth.model;
+
+import fr.univlille.labyrinth.model.gamemode.ProgressionMode;
+import fr.univlille.labyrinth.model.save.Challenge;
+import fr.univlille.labyrinth.model.save.Player;
+import fr.univlille.labyrinth.utils.Timer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgressionModeTest {
+
+    private Player player;
+    private Challenge challenge;
+    private ProgressionMode progressionMode;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("TestPlayer");
+        challenge = player.getProgress().getLevelProgress()[0].getChallenges()[2];
+        progressionMode = new ProgressionMode(player, challenge);
+    }
+
+    @Test
+    void testConstructorAndGetPlayer() {
+        assertNotNull(progressionMode.getPlayer());
+        assertEquals("TestPlayer", progressionMode.getPlayer().getName());
+        assertSame(player, progressionMode.getPlayer());
+    }
+
+    @Test
+    void testHandleVictoryMarksChallenge() throws Exception {
+        assertFalse(challenge.isCompleted());
+
+        Method handleVictory = ProgressionMode.class.getDeclaredMethod("handleVictory");
+        handleVictory.setAccessible(true);
+        handleVictory.invoke(progressionMode);
+
+        assertTrue(challenge.isCompleted());
+    }
+
+    @Test
+    void testHandleVictoryRecordsTime() throws Exception {
+        Timer timer = new Timer();
+        timer.start();
+        Thread.sleep(100);
+        timer.stop();
+
+        progressionMode.setChronometre(timer);
+        long recordedTime = timer.getChrono();
+
+        Method handleVictory = ProgressionMode.class.getDeclaredMethod("handleVictory");
+        handleVictory.setAccessible(true);
+        handleVictory.invoke(progressionMode);
+
+        assertTrue(challenge.getTimeCompleted() > 0);
+        assertEquals(recordedTime, challenge.getTimeCompleted());
+    }
+
+    @Test
+    void testHandleVictoryKeepsBestTime() throws Exception {
+        Timer timer1 = new Timer();
+        timer1.start();
+        Thread.sleep(200);
+        timer1.stop();
+
+        progressionMode.setChronometre(timer1);
+        Method handleVictory = ProgressionMode.class.getDeclaredMethod("handleVictory");
+        handleVictory.setAccessible(true);
+        handleVictory.invoke(progressionMode);
+
+        long firstTime = challenge.getTimeCompleted();
+        assertTrue(firstTime >= 200);
+
+        Timer timer2 = new Timer();
+        timer2.start();
+        Thread.sleep(50);
+        timer2.stop();
+
+        progressionMode.setChronometre(timer2);
+        handleVictory.invoke(progressionMode);
+
+        assertTrue(challenge.getTimeCompleted() < firstTime);
+        assertTrue(challenge.getTimeCompleted() >= 50);
+    }
+}
