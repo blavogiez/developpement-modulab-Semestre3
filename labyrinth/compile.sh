c4afc2b76d591b33e6a7514f4dab90e36bfa97ba
marouze <antonin.marouze.etu@univ-lille.fr>
2025-10-24 18:59:46 +0200

    change compile repertoire > target

--- labyrinth/compile.sh
+++ labyrinth/compile.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-javac --module-path "/usr/share/openjfx/lib" \
-      --add-modules javafx.controls,javafx.fxml \
-      -d "out" $(find src/main/java -name "*.java")
-cp -r src/main/resources/* "out"/ 2>/dev/null
-jar cfm "labyrinthEXC.jar" MANIFEST.MF -C "out" .
-echo "Compilation terminée"
+TARGET_DIR="target/classes"
+rm -rf "$TARGET_DIR"
+mkdir -p "$TARGET_DIR"
+javac \
+  --module-path "/usr/share/openjfx/lib" \
+  --add-modules javafx.controls,javafx.fxml \
+  -d "$TARGET_DIR" \
+  $(find "src/main/java" -name "*.java")
+cp -r "src/main/resources"/* "$TARGET_DIR"/ 2>/dev/null || true
+jar cfm "target/labyrinthEXC.jar" "MANIFEST.MF" -C "$TARGET_DIR" .
+echo "Compilation terminée "
