e8a3af9da881dc210acaf715f90f32e235ef1091
Zheng <angel.zheng.etu@univ-lille.fr>
2025-11-04 12:59:48 +0100

    fix color challenge completed

--- labyrinth/src/main/java/fr/univlille/labyrinth/model/algorithm/DFSAlgoMaze.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/model/algorithm/DFSAlgoMaze.java
@@ -14,13 +14,6 @@ public class DFSAlgoMaze {
         this.maze = maze;
     }
 
-//    public List<int[]> search(int[] coo1, int[] coo2){
-//        Stack<int[]> chemin =
-//        if(){
-//
-//        }
-//    }
-
     public List<int[]> voisins(int ligne, int colonne, boolean[][] visite){
         List<int[]> res = new ArrayList<>();
         if(ligne > 0 && !visite[ligne-1][colonne]){
