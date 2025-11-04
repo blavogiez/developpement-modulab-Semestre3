c3ef05f7cf6b72aa06f56a20430be29c5e333ee8
Antonin Marouze <antonin.marouze.etu@univ-lille.fr>
2025-11-04 13:26:27 +0100

    change listView to tableView

--- labyrinth/src/main/java/fr/univlille/labyrinth/controller/progressionmode/PlayerRankingControlleur.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/controller/progressionmode/PlayerRankingControlleur.java
@@ -7,20 +7,37 @@ import java.util.List;
 import fr.univlille.labyrinth.App;
 import fr.univlille.labyrinth.model.save.Player;
 import fr.univlille.labyrinth.model.save.PlayerDatabase;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PlayerRankingControlleur {
 
-    private List<Player> ranking= PlayerDatabase.loadAllPlayers();
+    private final List<Player> ranking= PlayerDatabase.loadAllPlayers();
 
     @FXML
-    private ListView<Player> classement;
+    private TableView<Player> tableView;
+
+    @FXML
+    private TableColumn<Player, String> colName;
+
+    @FXML
+    private TableColumn<Player, Integer> colScore;
+
 
     @FXML
     public void initialize(){
         Collections.sort(ranking);
-        classement.getItems().setAll(ranking);
+
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colScore.setCellValueFactory(new PropertyValueFactory<>("score"));
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        colName.prefWidthProperty().bind(tableView.widthProperty().multiply(0.5));
+        colScore.prefWidthProperty().bind(tableView.widthProperty().multiply(0.5));
+        tableView.setItems(FXCollections.observableArrayList(ranking));
     }
 
     /**
