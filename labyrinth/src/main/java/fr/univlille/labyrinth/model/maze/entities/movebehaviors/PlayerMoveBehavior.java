f2e0530d8e61b7e3d179cbdcd728d6c156e1bb84
Baptiste Lavogiez <baptiste.lavogiez.etu@univ-lille.fr>
2025-11-06 12:46:49 +0100

    chore: back avant effacement master

---
+++ labyrinth/src/main/java/fr/univlille/labyrinth/model/maze/entities/movebehaviors/PlayerMoveBehavior.java
@@ -0,0 +1,24 @@
+package fr.univlille.labyrinth.model.maze.entities.movebehaviors;
+
+import fr.univlille.labyrinth.model.maze.Maze;
+import fr.univlille.labyrinth.model.maze.Position;
+import fr.univlille.labyrinth.model.maze.entities.Entity;
+
+public class PlayerMoveBehavior implements MoveBehavior {
+    public void move(Entity entity, Maze maze) {
+        Position playerPosition = entity.getPosition();
+        Position newPos = new Position(pos.getY(), pos.getX());
+
+        maze.notifyObserver();
+        
+    }
+
+    /**
+     * Cette méthode permet de diriger le joueur vers une direction
+     *
+     * @param direction une direction parmi haut, bas, droite, gauche.
+     */
+    public void movePlayer(Direction direction){
+
+    }
+}
