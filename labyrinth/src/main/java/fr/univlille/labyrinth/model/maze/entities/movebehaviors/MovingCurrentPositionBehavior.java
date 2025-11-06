90d714b523bdfc6b4017e65aefdb1c5d1dbb2874
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-06 21:52:46 +0100

    ajout de MovingCurrentPositionBehavior

---
+++ labyrinth/src/main/java/fr/univlille/labyrinth/model/maze/entities/movebehaviors/MovingCurrentPositionBehavior.java
@@ -0,0 +1,35 @@
+package fr.univlille.labyrinth.model.maze.entities.movebehaviors;
+
+import java.util.List;
+import java.util.Random;
+
+import fr.univlille.labyrinth.model.algorithm.pathsearch.BreadthFirstSearch;
+import fr.univlille.labyrinth.model.maze.Direction;
+import fr.univlille.labyrinth.model.maze.Maze;
+import fr.univlille.labyrinth.model.maze.Position;
+import fr.univlille.labyrinth.model.maze.entities.Entity;
+
+/*
+ * Déplace l'entité de pas Distance actuel à sortie dans le labyrinthe (modifie sa position)
+ */
+public class MovingCurrentPositionBehavior implements MoveBehavior {
+    private Random random;
+
+    public MovingCurrentPositionBehavior() {
+    }
+
+    @Override
+    public void move(Entity entity, Direction direction, Maze maze) {
+            movingExitByCurrentDistance(entity,maze);
+    }
+
+    /*
+     * Déplace l'entité de pas Distance actuel  à sortie dans le labyrinthe (modifie sa position)
+     */
+    public void movingExitByCurrentDistance(Entity entity, Maze maze) {
+        int currentDistanceBetweenPlayerAndExit = BreadthFirstSearch.calculateDistance(maze, entity.getPosition(),maze.getExitPosition());
+        List<Position> candidates = BreadthFirstSearch.calculateAllDistances(maze, entity.getPosition(),currentDistanceBetweenPlayerAndExit);
+        Position exitPosition = candidates.get(random.nextInt(candidates.size()));
+        maze.setExit(exitPosition);
+    }
+}
