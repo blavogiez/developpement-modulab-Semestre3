5f383f1f39d1e4ab63f0c5b9cc73eda58d87dd8e
Romain Harlaut <romain.harlaut.etu@univ-lille.fr>
2025-11-20 11:07:25 +0100

    fix doc non genere

--- labyrinth/src/main/java/fr/univlille/labyrinth/model/maze/traps/trap/Trap.java
+++ labyrinth/src/main/java/fr/univlille/labyrinth/model/maze/traps/trap/Trap.java
@@ -11,7 +11,7 @@ public abstract class Trap {
     Position position;
 
     /**
-     * Action effectuée lorsque le piège est utilisé par un joueur.
+     * Action effectuée lorsqu'un joueur déclenche le piège.
      *
      * @param playerID    l'identifiant du joueur déclenchant le piège
      * @param position    la position actuelle du joueur
@@ -20,15 +20,14 @@ public abstract class Trap {
      */
     public abstract void onUse(int playerID, Position position, Position oldPosition, ObservableMaze maze);
 
-
     /**
      * Remplace le piège à la position donnée par un piège "utilisé".
      *
      * @param position la position du piège à révéler
      * @param traps    le tableau de pièges du labyrinthe
      */
-    protected void revealTrap(Position positon, Trap[][] traps) {
-        traps[positon.getY()][positon.getX()] = TrapFactory.USED.generateTrap();
+    protected void revealTrap(Position position, Trap[][] traps) {
+        traps[position.getY()][position.getX()] = TrapFactory.USED.generateTrap();
     }
 
     /**
@@ -36,17 +35,17 @@ public abstract class Trap {
      *
      * @return le nom du piège sous forme de {@code String}
      */
-    public String name(){
+    public String name() {
         return "NONE";
     }
 
     /**
-     * Renvoie une position aléatoire libre dans le labyrinthe pour placer un piège.
+     * Renvoie une position libre aléatoire dans le labyrinthe pour placer un piège.
      *
      * @param maze le labyrinthe dans lequel chercher une cellule libre
      * @return une position libre aléatoire
      */
-    protected static Position getRandomCell(ObservableMaze maze){
+    protected static Position getRandomCell(ObservableMaze maze) {
         Random random = new Random();
         Trap[][] traps = maze.getTrapManager().getTraps();
         EntityManager entityManager = maze.getEntityManager();
@@ -56,21 +55,22 @@ public abstract class Trap {
             y = random.nextInt(traps.length);
             x = random.nextInt(traps[y].length);
             position = new Position(x, y);
-        } while (!isFree(traps,entityManager,position));
+        } while (!isFree(traps, entityManager, position));
         return position;
     }
 
     /**
      * Vérifie si une position est libre pour y placer un piège.
+     * <p>
      * Une position est libre si elle contient un {@code NoneTrap} et qu'aucune entité ne s'y trouve.
      *
-     * @param traps          le tableau de pièges du labyrinthe
-     * @param entityManager  le gestionnaire des entités du labyrinthe
-     * @param position       la position à tester
+     * @param traps         le tableau de pièges du labyrinthe
+     * @param entityManager le gestionnaire des entités du labyrinthe
+     * @param position      la position à tester
      * @return {@code true} si la position est libre, sinon {@code false}
      */
-    private static boolean isFree(Trap[][] traps, EntityManager entitymanager, Position position){
-        return traps[position.getY()][position.getX()] instanceof NoneTrap && !entitymanager.isEntityOnCell(position);
+    private static boolean isFree(Trap[][] traps, EntityManager entityManager, Position position) {
+        return traps[position.getY()][position.getX()] instanceof NoneTrap
+                && !entityManager.isEntityOnCell(position);
     }
-
 }
